6f42ad16132ba8d99ff5700c3a7b45046bb145c0
noreplydev <contact.cristiansanchez@gmail.com>
2024-06-04 23:55:09 +0200

    Implement protobuf service definition into an actual rust type

--- services/rust/src/main.rs
+++ services/rust/src/main.rs
@@ -1,7 +1,25 @@
-mod entity {
+pub mod entity {
     tonic::include_proto!("entity"); // the parameter here is the package name specified in your .proto file
 }
 
-fn main() {
-    println!("Hello, world!");
+use entity::entity_provider_server::{EntityProvider, EntityProviderServer};
+use entity::{Entity, EntityType};
+use tonic::{Request, Response, Status};
+
+struct MyEntityProvider;
+
+#[tonic::async_trait]
+impl EntityProvider for MyEntityProvider {
+    async fn generate_entity(
+        &self,
+        request: Request<EntityType>,
+    ) -> Result<Response<Entity>, Status> {
+        Ok(Response::new(Entity {
+            data: "Hello".to_string(),
+            r#type: "Hello".to_string(),
+            index: 0,
+        }))
+    }
 }
+
+fn main() {}
